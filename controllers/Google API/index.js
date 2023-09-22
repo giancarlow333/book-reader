dd6db6b43f189055fc856bd1a35f8d729b8b6baf
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-09-21 22:02:45 -0700

    adding API & node wrapper

---
+++ controllers/Google API/index.js
@@ -0,0 +1,29 @@
+var books = require('google-books-search');
+
+
+
+books.search('Lackey', function(error, results) {
+    if ( ! error ) {
+        console.log(results);
+    } else {
+        console.log(error);
+    }
+});
+
+books.lookup('9KJJYFIss_wC', function(error, results) {
+    if ( ! error ) {
+        console.log(results);
+    } else {
+        console.log(error);
+    }
+});
+
+var options = {
+    key: "YOUR API KEY",
+    field: 'title',
+    offset: 0,
+    limit: 10,
+    type: 'books',
+    order: 'relevance',
+    lang: 'en'
+};
