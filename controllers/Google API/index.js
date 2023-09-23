39c40690b15624400f014fe93a7f9b8613500dbd
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-09-23 16:18:51 -0700

    testing API

--- controllers/Google API/index.js
+++ controllers/Google API/index.js
@@ -2,7 +2,7 @@ var books = require('google-books-search');
 
 
 
-books.search('Lackey', function(error, results) {
+books.search('Darkness Lackey', function(error, results) {
     if ( ! error ) {
         console.log(results);
     } else {
@@ -10,23 +10,23 @@ books.search('Lackey', function(error, results) {
     }
 });
 
-books.lookup('9KJJYFIss_wC', function(error, results) {
-    if ( ! error ) {
-        console.log(results);
-    } else {
-        console.log(error);
-    }
-});
+//books.lookup('9KJJYFIss_wC', function(error, results) {
+   // if ( ! error ) {
+     //   console.log(results);
+  //  } else {
+    //    console.log(error);
+ //   }
+//});
 
 //code codee
-var options = {
-    key: "YOUR API KEY",
-    field: 'title',
-    offset: 0,
-    limit: 10,
-    type: 'books',
-    order: 'relevance',
-    lang: 'en'
-};
+//var options = {
+   // key: "YOUR API KEY",
+   // field: 'title',
+  //  offset: 0,
+  //  limit: 10,
+  //  type: 'books',
+  //  order: 'relevance',
+  //  lang: 'en'
+//};
 
 
