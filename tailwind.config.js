cadb3e0ee51ebc14db7db7efdd3aa7da9fc25126
ayrh90-fZNzxW <ayrh90@icloud.com>
2023-09-25 19:51:55 -0700

    handlebars

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     './index.html',
     './SignUp.html',
     './search.html',
-    './models/avators.js',
-
+    './public/css/js/avators.js',
+    './public/css/js/quickAddBtn.js',
+    './views/layouts/main.handlebars',
+    './views/index.handlebars',
+    './views/partials/bookDetails.handlebars',
+    './views/partials/login.handlebars',
+    './views/partials/results.handlebars',
+    './views/partials/signup.handlebars',
+    './views/index.handlebars',
   ],
   theme: {
     extend: {},
