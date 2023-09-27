b5075bd1838b1345150f4b091af2a16abdb5a750
ayrh90-fZNzxW <ayrh90@icloud.com>
2023-09-27 04:18:31 -0700

    list handlebar

--- controllers/index.js
+++ controllers/index.js
@@ -8,3 +8,5 @@ module.exports = router;
 
 
 //testing
+
+//testing handlebars 
--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+const path = require('path'); 
 const express = require('express');
-const exphbs =require('express-handlebars'); //handlebars express
+
+//-----handlebars------
+const exphbs =require('express-handlebars');
+const hbs = exphbs.create({});
+
+//-------------
+
 const sequelize = require('./config');
-const path = require('path'); // handlebars 
 const routes = require('./routes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// inititalize and istance of HANDLEBARS 
-const hbs = exphbs.create({});
-// handlebars middleware set up
+//------inititalize and istance of HANDLEBARS ------
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/index.js'))
+//-----------------------------------
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+
+
 
 // We are going to build our folders and APS's in such a way where our folder strucatures will math ourendpoints.  This is accomplished by using "routing middleware"
 
