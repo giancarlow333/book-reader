6873afff7c3851605a334db011f7ce9af615034c
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-09-28 17:01:56 -0700

    "Resolved a login and sign up bug...god mode only"

--- controllers/auth-routes.js
+++ controllers/auth-routes.js
@@ -6,32 +6,35 @@ const { Op } = require('sequelize');
 
 //localhost:3001/auth/
 
-router.post('/signup', async (req, res) => {
-    const {
-        username,
-        name,
-        email,
-        password,
-        reEnterPassword,
-    } = req.body;
-
-    //might need to add additional parameters for reEnterPassword
-    if (!username || !email || !password) {
-        //return res.status(400).json({ error: 'You must provider username, first name, last name, password, and email' });
-        res.redirect('../signup');
-    }
-
-	console.log("Sign Up Route 2");
-
+router.post('/signup', async (req, res, next) => {
     // PaSsWoRd
     try {
+		const {
+			username,
+			name,
+			email,
+			password,
+			confirmation,
+		} = req.body;
+	
+		//might need to add additional parameters for reEnterPassword
+		if (!username || !email || !password || !confirmation || !name) {
+			//return res.status(400).json({ error: 'You must provider username, first name, last name, password, and email' });
+			res.redirect('../signup');
+			return;
+		}
+	
+		if(password !== confirmation){
+			console.log("Password mismatch: " + password + ", "+ confirmation);
+			res.redirect('../signup');
+			return;
+		}
+
         const newUser = await User.create({
             name,
             username,
             email,
             password,
-            reEnterPassword,
-
         });
         console.log(newUser);
 
@@ -41,25 +44,31 @@ router.post('/signup', async (req, res) => {
 		console.log("success");
 
         res.redirect('../results');
+		return;
     } catch (e) {
         console.log(e);
+		// this is where you log to a file
         // res.json(e);
         res.redirect('../signup');
+		return;
     }
 
 });
 router.get('/logout', async (req, res) => {
     req.session.destroy();
     res.redirect('../')
+	return;
 });
 
 router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        //return res.status(401).json({ error: 'You must provide a valid username and password' });
-        res.redirect('../');
-    }
     try {
+		const { username, password } = req.body;
+		if (!username || !password) {
+			//return res.status(401).json({ error: 'You must provide a valid username and password' });
+			res.redirect('../');
+			return;
+		}
+
         //	find the user with the given email
         const user = await User.findOne({
             where: {
@@ -71,6 +80,7 @@ router.post('/login', async (req, res) => {
             //	if no user exists, give them a 400
             //return res.status(400).json({ error: 'No user with that username' });
             res.redirect('../');
+			return;
         }
         //	take the users hashed password and compare it with the password that they're passing in from the form
         const isMatching = await bcrypt.compare(password, user.password)
@@ -78,14 +88,17 @@ router.post('/login', async (req, res) => {
         if (!isMatching) {
             //return res.status(401).json({ error: 'Invalid password' });
             res.redirect('../');
+			return;
         }
         //return res.status(200).json({ message: 'You are now logged in successfully' });
         CreateSessionData(req, user);
 
         res.redirect('../results');
+		return;
     } catch (e) {
         console.log(e);
         res.redirect('../');
+		return;
         //return res.status(200).json(e);
     }
 });
