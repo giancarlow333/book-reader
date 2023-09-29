c9e100e698e20ac898691532ca7e306d1ebdeba8
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-09-28 17:07:59 -0700

    "added console.logs"

--- controllers/auth-routes.js
+++ controllers/auth-routes.js
@@ -7,8 +7,8 @@ const { Op } = require('sequelize');
 //localhost:3001/auth/
 
 router.post('/signup', async (req, res, next) => {
-    // PaSsWoRd
-    try {
+	// PaSsWoRd
+	try {
 		const {
 			username,
 			name,
@@ -16,97 +16,103 @@ router.post('/signup', async (req, res, next) => {
 			password,
 			confirmation,
 		} = req.body;
-	
+
 		//might need to add additional parameters for reEnterPassword
 		if (!username || !email || !password || !confirmation || !name) {
 			//return res.status(400).json({ error: 'You must provider username, first name, last name, password, and email' });
-			res.redirect('../signup');
-			return;
-		}
-	
-		if(password !== confirmation){
-			console.log("Password mismatch: " + password + ", "+ confirmation);
+			console.log("missing fields");
 			res.redirect('../signup');
 			return;
 		}
 
-        const newUser = await User.create({
-            name,
-            username,
-            email,
-            password,
-        });
-        console.log(newUser);
+		if (password !== confirmation) {
+			console.log("Password mismatch: " + password + ", " + confirmation);
+			res.redirect('../signup');
+			return;
+		}
 
-        //set session data
-        CreateSessionData(req, newUser);
+		const newUser = await User.create({
+			name,
+			username,
+			email,
+			password,
+		});
+		console.log(newUser);
+
+		//set session data
+		CreateSessionData(req, newUser);
 
 		console.log("success");
 
-        res.redirect('../results');
+		res.redirect('../results');
 		return;
-    } catch (e) {
-        console.log(e);
+	} catch (e) {
+		console.log(e);
 		// this is where you log to a file
-        // res.json(e);
-        res.redirect('../signup');
+		// res.json(e);
+		console.log(e);
+		res.redirect('../signup');
 		return;
-    }
+	}
 
 });
 router.get('/logout', async (req, res) => {
-    req.session.destroy();
-    res.redirect('../')
+	req.session.destroy();
+	res.redirect('../')
 	return;
 });
 
 router.post('/login', async (req, res) => {
-    try {
+	try {
 		const { username, password } = req.body;
 		if (!username || !password) {
 			//return res.status(401).json({ error: 'You must provide a valid username and password' });
+			console.log("Missing a username or a password");
 			res.redirect('../');
 			return;
 		}
 
-        //	find the user with the given email
-        const user = await User.findOne({
-            where: {
-                username,
-            }
-        });
-        //	check if the user actually exists with that given email
-        if (user == null) {
-            //	if no user exists, give them a 400
-            //return res.status(400).json({ error: 'No user with that username' });
-            res.redirect('../');
+		//	find the user with the given email
+		const user = await User.findOne({
+			where: {
+				username,
+			}
+		});
+		//	check if the user actually exists with that given email
+		if (user == null) {
+			//	if no user exists, give them a 400
+			//return res.status(400).json({ error: 'No user with that username' });
+			console.log("MUser does not exist");
+			res.redirect('../');
 			return;
-        }
-        //	take the users hashed password and compare it with the password that they're passing in from the form
-        const isMatching = await bcrypt.compare(password, user.password)
-        console.log(isMatching);
-        if (!isMatching) {
-            //return res.status(401).json({ error: 'Invalid password' });
-            res.redirect('../');
+		}
+		//	take the users hashed password and compare it with the password that they're passing in from the form
+		const isMatching = await bcrypt.compare(password, user.password)
+		console.log(isMatching);
+		if (!isMatching) {
+			//return res.status(401).json({ error: 'Invalid password' });
+			console.log("Incorrect password");
+			res.redirect('../');
 			return;
-        }
-        //return res.status(200).json({ message: 'You are now logged in successfully' });
-        CreateSessionData(req, user);
+		}
+		//return res.status(200).json({ message: 'You are now logged in successfully' });
+		CreateSessionData(req, user);
 
-        res.redirect('../results');
+		console.log("success");
+		res.redirect('../results');
 		return;
-    } catch (e) {
-        console.log(e);
-        res.redirect('../');
+	} catch (e) {
+		console.log(e);
+		res.redirect('../');
 		return;
-        //return res.status(200).json(e);
-    }
+		//return res.status(200).json(e);
+	}
 });
 
 function CreateSessionData(req, user) {
-    //session variables
-    req.session.username = user.username;
-    req.session.email = user.email;
+	//session variables
+	req.session.username = user.username;
+	req.session.email = user.email;
 }
 
 
