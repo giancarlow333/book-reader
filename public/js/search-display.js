10e842d5502cfd127decf4912626be566912911e
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-10-14 20:46:05 -0700

    Revert HerokuAPI change

--- public/js/search-display.js
+++ public/js/search-display.js
@@ -8,7 +8,7 @@ var listSelect = document.querySelectorAll("#listSelect");
 var resultsBox = document.querySelector("#results");
 
 //const herokuAPI = "https://ravishing-reads-a2209ea97ad8.herokuapp.com/";
-//const herokuAPI = "http://localhost:3001/";
+const herokuAPI = "http://localhost:3001/";
 
 console.log(searchData);
 
